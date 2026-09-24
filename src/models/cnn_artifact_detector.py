06bbb90480456e16f8f80136d36098cf423d98da
Fer Andrade <and22676@uvg.edu.gt>
2026-09-24 08:59:12 -0600

    General fixes

--- src/models/cnn_artifact_detector.py
+++ src/models/cnn_artifact_detector.py
@@ -299,7 +299,7 @@ class EEGWindowDataset(Dataset):
             q75, q25 = np.percentile(x, [75,25], axis=1)
             scale = np.maximum((q75 - q25)[:, None]/1.349, 1e-12)
             s["_norm"] = (med.astype(np.float32), scale.astype(np.float32))
-        return s["norm"]
+        return s["_norm"]
     
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
